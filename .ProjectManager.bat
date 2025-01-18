2bb4639b808f30ecb6402b7a04cb6329bf134a0a
Elias Johnson <eliascarljohnson@gmail.com>
2025-01-18 15:42:27 -0800

    Added Project Manager script

---
+++ .ProjectManager.bat
@@ -0,0 +1,10 @@
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: NAME	       : Project Manager
+::
+:: DESCRIPTION : CLI for accessing and managing your projects.
+:: VERSION     : 1.0.0
+::
+:: AUTHOR      : Elias Johnson
+:: DATE        : 1-18-24
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
